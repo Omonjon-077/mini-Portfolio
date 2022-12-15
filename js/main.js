72b7abbd4bcda632f6e3a0f30a0082e43d4da09a
Omonjon <omonjons077@gmail.com>
2022-12-15 15:59:31 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,81 @@
+/*=============== FILTERS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tc => {  /*tc = tabcontent*/
+            tc.classList.remove('filters__active')
+        })
+        target.classList.add('filters__active')
+    })
+})
+
+/*=============== FILTERS TABS (Active) ===============*/
+const projectBtn = document.getElementById('project-btn'),
+    skillsBtn = document.getElementById('skills-btn')
+
+if (projectBtn) {
+    projectBtn.addEventListener('click', () => {
+        projectBtn.classList.add('active-btn')
+        skillsBtn.classList.remove('active-btn')
+    })
+}
+if (skillsBtn) {
+    skillsBtn.addEventListener('click', () => {
+        skillsBtn.classList.add('active-btn')
+        projectBtn.classList.remove('active-btn')
+    })
+}
+
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal('.profile__border')
+sr.reveal('.profile__name, .profile__profession, .profile__social, .profile__buttons, .filters__content, .filters__content, .filters', { delay: 500 })
+sr.reveal('.profile__info-group, .projects__card', { interval: 200 })
